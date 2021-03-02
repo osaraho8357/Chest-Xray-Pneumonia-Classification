aa2ed05d512913047f9d887a1063d7685323773f
sebbyu <you.sebastian.1.618@gmail.com>
2021-03-02 02:55:18 -0800

    Home Component Created

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,16 @@
+<template lang='pug'>
+p hello world
+</template>
+
+
+<script lang='ts'>
+import {defineComponent} from 'vue'
+export default defineComponent({
+  name: "HomeComponent",
+})
+</script>
+
+
+<style lang='stylus' scoped>
+
+</style>
